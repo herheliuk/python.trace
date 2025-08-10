67fe9e0d96518c76547984c54f02d02e61aad2e4
AuthorName <Author@email.com>
2025-08-11 00:41:57 +0100

    pushed

--- _tracetracer.py
+++ _tracetracer.py
@@ -44,6 +44,8 @@ def filter_scope(scope: dict) -> dict:
     return {key: value for key, value in scope.items() if not startswith(key, "__")}
 
 def diff_scope(old_scope: dict, new_scope: dict) -> dict:
+    if old_scope is new_scope:
+        return {}
     changes = {key: value for key, value in new_scope.items() if old_scope.get(key) != value}
     deleted = {key: "<deleted>" for key in old_scope.keys() - new_scope.keys()}
     return {**changes, **deleted}
