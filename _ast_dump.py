dc371d919255e257240ef0e132ef609fe7b876fe
AuthorName <Author@email.com>
2025-08-13 14:30:39 +0100

    pushed

--- _ast_dump.py
+++ _ast_dump.py
@@ -6,16 +6,16 @@ from pathlib import Path
 if __name__ == '__main__':
     assert len(sys.argv) == 2, f'Usage: python {sys.argv[0]} <script to dump>'
 
-    dump_script_path = Path(sys.argv[1]).resolve()
+    script_path = Path(sys.argv[1]).resolve()
 
-    assert dump_script_path.exists(), f'File "{dump_script_path.name}" does not exist.'
+    assert script_path.exists(), f'File "{script_path.name}" does not exist.'
 
-    source_code = dump_script_path.read_text()
+    source_code = script_path.read_text()
     
-    ast_tree = ast.parse(source_code)
+    ast_tree = ast.parse(source_code, filename=script_path.name)
     
     tree_dump = ast.dump(ast_tree, indent=4)
 
-    output_file = Path.cwd() / (dump_script_path.stem + '.ast.txt')
+    output_file = Path.cwd() / (script_path.stem + '.ast.txt')
 
     output_file.write_text(tree_dump)
